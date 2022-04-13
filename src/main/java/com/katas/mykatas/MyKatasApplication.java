63a34655d786cf5c58893d03a9c9b8d2044d53f5
Marius Moraru (TME) <marius.moraru@external.toyota-europe.com>
2022-04-13 10:03:58 +0300

    Initial commit

---
+++ src/main/java/com/katas/mykatas/MyKatasApplication.java
@@ -0,0 +1,13 @@
+package com.katas.mykatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKatasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKatasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/katas/mykatas/MykatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katas.mykatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykatasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
